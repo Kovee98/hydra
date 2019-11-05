7e88f894b3a34cecd226cd72fb928ba87a7749c9
Joshua <jlkovalchi42@students.tntech.edu>
2019-11-05 07:27:22 -0600

    Adds color sample icon.

--- src/components/SyntaxColor.vue
+++ src/components/SyntaxColor.vue
@@ -1,9 +1,9 @@
 <template>
     <q-input outlined v-model="currentColor" :rules="['anyColor']" :label="label">
         <template v-slot:append>
-            <q-icon name="colorize" class="cursor-pointer">
+            <q-icon ref="sample" name="fas fa-square" class="cursor-pointer" :style="style">
                 <q-popup-proxy transition-show="scale" transition-hide="scale">
-                    <q-color v-model="currentColor" />
+                    <q-color v-model="currentColor" @input="input" />
                 </q-popup-proxy>
             </q-icon>
         </template>
@@ -15,8 +15,19 @@ export default {
     props: ['color', 'label'],
     data () {
         return {
-            currentColor: this.color
+            currentColor: this.color,
+            style: {
+                color: this.color
+            }
         };
+    },
+    methods: {
+        input () {
+            console.log('this.currentColor:', this.currentColor);
+            this.style = {
+                color: this.currentColor
+            };
+        }
     }
 };
 </script>

39415a83d4c28fbc3bbddd32778ee63db93486e5
Joshua Kovalchik <jkovalchik@averittexpress.com>
2019-11-13 16:58:34 -0600

    Removes comment.

--- src/store/settings/mutations.js
+++ src/store/settings/mutations.js
@@ -1,5 +1,4 @@
 export function update (state, settings) {
-    // state = settings;
     state.colors = settings.colors;
     state.history = settings.history;
     state.notifications = settings.notifications;

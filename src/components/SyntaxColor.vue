17fe66da558dbe088aa1343d144bade08409d248
Joshua <jlkovalchi42@students.tntech.edu>
2019-11-17 07:40:09 -0600

    Fixes missing icon issue with color pickers.

--- src/components/SyntaxColor.vue
+++ src/components/SyntaxColor.vue
@@ -1,7 +1,7 @@
 <template>
     <q-input filled v-model="value.color" :rules="['anyColor']" :label="value.type">
         <template v-slot:append>
-            <q-icon ref="sample" name="fas fa-square" class="cursor-pointer" :style="style">
+            <q-icon ref="sample" name="colorize" class="cursor-pointer" :style="style">
                 <q-popup-proxy transition-show="scale" transition-hide="scale">
                     <q-color v-model="value.color" />
                 </q-popup-proxy>

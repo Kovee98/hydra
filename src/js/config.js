9cc3a3174b1ea8ebe34b44da2af9cb6aca007b9e
Joshua <jlkovalchi42@students.tntech.edu>
2019-11-29 10:44:47 -0600

    Adds bracket matching/highlighting. Adds autoclosing of brackets.

--- src/js/config.js
+++ src/js/config.js
@@ -1,16 +1,15 @@
 var codemirror = {
     tabSize: 2,
-    mode: {
-        name: 'javascript',
-        json: true
-    },
+    mode: 'application/json',
     lineNumbers: true,
     smartIndent: true,
     lineWrapping: true,
     lineWiseCopyCut: false,
     cursorHeight: 0.85,
     scrollbarStyle: 'null',
-    showCursorWhenSelecting: true
+    showCursorWhenSelecting: true,
+    autoCloseBrackets: true,
+    matchBrackets: true
 };
 
 export {

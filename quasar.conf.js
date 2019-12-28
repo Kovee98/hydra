22d4a70d66ccfc6b4f3679071cc206dc04be1a23
Joshua Kovalchik <kovee98@gmail.com>
2019-12-28 14:23:25 -0800

    Adds nodeIntegration in config.

--- quasar.conf.js
+++ quasar.conf.js
@@ -94,51 +94,8 @@ module.exports = function (ctx) {
         // https://quasar.dev/options/animations
         // animations: [],
 
-        // https://quasar.dev/quasar-cli/developing-ssr/configuring-ssr
-        ssr: {
-            pwa: false
-        },
-
-        // https://quasar.dev/quasar-cli/developing-pwa/configuring-pwa
-        pwa: {
-            // workboxPluginMode: 'InjectManifest',
-            // workboxOptions: {}, // only for NON InjectManifest
-            manifest: {
-                // name: 'Hydra',
-                // short_name: 'Hydra',
-                // description: 'A minimal Http client.',
-                display: 'standalone',
-                orientation: 'portrait',
-                background_color: '#ffffff',
-                theme_color: '#027be3',
-                icons: [
-                    {
-                        'src': 'statics/icons/icon-128x128.png',
-                        'sizes': '128x128',
-                        'type': 'image/png'
-                    },
-                    {
-                        'src': 'statics/icons/icon-192x192.png',
-                        'sizes': '192x192',
-                        'type': 'image/png'
-                    },
-                    {
-                        'src': 'statics/icons/icon-256x256.png',
-                        'sizes': '256x256',
-                        'type': 'image/png'
-                    },
-                    {
-                        'src': 'statics/icons/icon-384x384.png',
-                        'sizes': '384x384',
-                        'type': 'image/png'
-                    },
-                    {
-                        'src': 'statics/icons/icon-512x512.png',
-                        'sizes': '512x512',
-                        'type': 'image/png'
-                    }
-                ]
-            }
+        electron: {
+            nodeIntegration: true
         }
     };
 };
